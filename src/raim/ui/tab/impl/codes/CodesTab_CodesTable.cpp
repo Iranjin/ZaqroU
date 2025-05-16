da4d6f62a9b38889531ec712b13c8566502b638c
Iranjin <iranjin262@gmail.com>
2025-05-16 20:21:43 +0900

    パディング修正

--- src/raim/ui/tab/impl/codes/CodesTab_CodesTable.cpp
+++ src/raim/ui/tab/impl/codes/CodesTab_CodesTable.cpp
@@ -10,25 +10,25 @@ void CodesTab::CodesTable(const ImVec2 &available)
     if (ImGui::BeginTable("CodesTable", 3, 
         ImGuiTableFlags_Borders | 
         ImGuiTableFlags_RowBg | 
-        ImGuiTableFlags_Resizable |
+        ImGuiTableFlags_Resizable | 
         ImGuiTableFlags_ScrollY,
         ImVec2(0, topHeight)))
     {
         ImGui::TableNextColumn();
-        ImGui::BeginChild("CodesSearchBarFrame", ImVec2(0, 0), ImGuiChildFlags_None);
+        ImGui::BeginChild("CodesSearchBarFrame");
         CodesFrame_Search();
-        ImGui::BeginChild("CodesFrame", ImVec2(0, 0), ImGuiChildFlags_None, ImGuiWindowFlags_AlwaysUseWindowPadding);
+        ImGui::BeginChild("CodesFrame");
         CodesFrame();
         ImGui::EndChild();
         ImGui::EndChild();
 
         ImGui::TableNextColumn();
-        ImGui::BeginChild("CodeFrame", ImVec2(0, 0), ImGuiChildFlags_None, ImGuiWindowFlags_AlwaysUseWindowPadding);
+        ImGui::BeginChild("CodeFrame");
         CodeFrame();
         ImGui::EndChild();
 
         ImGui::TableNextColumn();
-        ImGui::BeginChild("CommentFrame", ImVec2(0, 0), ImGuiChildFlags_None, ImGuiWindowFlags_AlwaysUseWindowPadding);
+        ImGui::BeginChild("CommentFrame");
         CommentFrame();
         ImGui::EndChild();
 
