07ce857855ef1f0259e8972b95a20f9121796a51
Iranjin <iranjin262@gmail.com>
2025-05-22 06:52:38 +0900

    main修正

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ static void glfw_error_callback(int error, const char *description)
     fprintf(stderr, "GLFW Error %d: %s\n", error, description);
 }
 
-int main()
+int main(int argc, char **argv)
 {
     glfwSetErrorCallback(glfw_error_callback);
     if (!glfwInit())
