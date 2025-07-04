3b8432465fb178ffe33d9c4a47b1e2d1d18b1007
Iranjin <iranjin262@gmail.com>
2025-07-04 22:48:36 +0900

    Make CodesTable responsive to available width
    Adjust the number of columns in CodesTable based on the available width. If the width is less than 500px, the table displays a single column; otherwise, it shows three columns. This improves the UI's adaptability to different window sizes.

--- src/raim/ui/tab/impl/codes/CodesTab_CodesTable.cpp
+++ src/raim/ui/tab/impl/codes/CodesTab_CodesTable.cpp
@@ -6,11 +6,14 @@
 void CodesTab::CodesTable(const ImVec2 &available)
 {
     float top_height = (available.y - ImGui::GetStyle().ItemSpacing.y) * 2.0f / 3.0f;
-    
-    if (ImGui::BeginTable("CodesTable", 3, 
-                          ImGuiTableFlags_Borders | 
-                          ImGuiTableFlags_RowBg | 
-                          ImGuiTableFlags_Resizable | 
+    constexpr float min_width_for_three_columns = 500.0f;
+
+    int column_count = (available.x > min_width_for_three_columns) ? 3 : 1;
+
+    if (ImGui::BeginTable("CodesTable", column_count,
+                          ImGuiTableFlags_Borders |
+                          ImGuiTableFlags_RowBg |
+                          ImGuiTableFlags_Resizable |
                           ImGuiTableFlags_ScrollY,
                           ImVec2(0, top_height)))
     {
@@ -22,15 +25,18 @@ void CodesTab::CodesTable(const ImVec2 &available)
         ImGui::EndChild();
         ImGui::EndChild();
 
-        ImGui::TableNextColumn();
-        ImGui::BeginChild("CodeFrame");
-        CodeFrame();
-        ImGui::EndChild();
+        if (column_count == 3)
+        {
+            ImGui::TableNextColumn();
+            ImGui::BeginChild("CodeFrame");
+            CodeFrame();
+            ImGui::EndChild();
 
-        ImGui::TableNextColumn();
-        ImGui::BeginChild("CommentFrame");
-        CommentFrame();
-        ImGui::EndChild();
+            ImGui::TableNextColumn();
+            ImGui::BeginChild("CommentFrame");
+            CommentFrame();
+            ImGui::EndChild();
+        }
 
         ImGui::EndTable();
     }
